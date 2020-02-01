bf9e431167c8e05b1cb98020bacecd9a8ad9dfa6
Lillian Freire <lillian.freire@gmail.com>
2020-02-01 14:05:27 -0300

    Estilização do componente relatorios com dados de exemplo do Angular Material

--- ABCDAirlines/src/app/app.module.ts
+++ ABCDAirlines/src/app/app.module.ts
@@ -20,7 +20,8 @@ import {
   MatSidenavModule,
   MatListModule,
   MatCheckboxModule,
-  MatSelectModule
+  MatSelectModule,
+  MatTableModule
 } from "@angular/material";
 
 @NgModule({
@@ -45,7 +46,8 @@ import {
     MatSidenavModule,
     MatListModule,
     MatCheckboxModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
